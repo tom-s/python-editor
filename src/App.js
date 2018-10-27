b9c130a248df84407eeace061c97052704704162
tom-s <thom.schell@gmail.com>
2018-10-27 02:00:22 +0200

    working example !

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import MonacoEditor from 'react-monaco-editor'
+import { conf, language } from './python'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      code: [
+        'def cook(val):',
+        '\tprint(val + " nut bread")',
+        'fruit = "banana"',
+        'cook(fruit)'
+      ].join('\n')
+    }
+  }
+  editorWillMount(monaco) {
+    monaco.languages.register({ 
+      id: "python-custom",
+      extensions: ['.py', '.rpy', '.pyw', '.cpy', '.gyp', '.gypi'],
+      aliases: ['Python', 'py'],
+     })
+    monaco.languages.setLanguageConfiguration('python-custom', conf)
+    monaco.languages.setMonarchTokensProvider("python-custom", language)
+  }
+
+  editorDidMount = (editor, monaco) => {
+    console.log('editorDidMount', editor)
+    editor.focus()
+  }
+  onChange = (newValue, e) => {
+    console.log('onChange', newValue, e)
+  }
   render() {
+    const code = this.state.code
+    const options = {
+      selectOnLineNumbers: true,
+      overviewRulerLanes: 1
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MonacoEditor
+          width="800"
+          height="600"
+          language="python-custom"
+          theme="vs-dark"
+          value={code}
+          options={options}
+          onChange={this.onChange}
+          /*
+          requireConfig={{
+            'vs/nls' : {
+              availableLanguages: {
+                '*': 'de'
+              }
+            }
+          }}*/
+          editorDidMount={this.editorDidMount}
+          editorWillMount={this.editorWillMount}
+        />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
