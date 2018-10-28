28604aa81579e0fcbd7f13386a5d42f9781ff2dc
tom-s <thom.schell@gmail.com>
2018-10-28 12:03:28 +0100

    add autocomplete option

--- src/monaco-editor/index.js
+++ src/monaco-editor/index.js
@@ -8,6 +8,7 @@ import { librariesKeywords } from './common'
 const LANGUAGE = 'python-custom'
 
 class Editor extends Component {
+  completionProviderDisposable = null
   editorWillMount(monaco) {
     monaco.languages.register({ 
       id: "python-custom",
@@ -22,13 +23,22 @@ class Editor extends Component {
       rules: [
         { token: 'library-import', foreground: '3CB371', fontStyle: 'underline' }
       ]
-  });
+    });
+  }
+
+  enableAutoComplete = monaco => {
+    this.completionProviderDisposable = monaco.languages.registerCompletionItemProvider(LANGUAGE, {
+      provideCompletionItems: (model) => createDependencyProposals(monaco, model.getValue())
+    })
+  }
+
+  disableAutoComplete = () => {
+    this.completionProviderDisposable && this.completionProviderDisposable.dispose()
+    this.completionProviderDisposable = null
   }
 
   editorDidMount = (editor, monaco) => {
-    monaco.languages.registerCompletionItemProvider(LANGUAGE, {
-      provideCompletionItems: (model) => createDependencyProposals(monaco, model.getValue())
-    })
+    this.enableAutoComplete(monaco)
     monaco.languages.registerHoverProvider(LANGUAGE, {
       provideHover: (model, position) => createHoverText(monaco, model, position)
     })
@@ -36,28 +46,44 @@ class Editor extends Component {
     // Customize context menu
     editor.addAction({
       // An unique identifier of the contributed action.
-      id: 'lls',
-    
+      id: 'enable-autocomplete',
       // A label of the action that will be presented to the user.
-      label: 'Vive lelivrescolaire.fr !',
-    
+      label: 'Enable autocomplete',
       // An optional array of keybindings for the action.
       keybindings: [],
-    
       // A precondition for this action.
       precondition: null,
-    
       // A rule to evaluate on top of the precondition in order to dispatch the keybindings.
       keybindingContext: null,
-    
-      contextMenuGroupId: 'navigation',
-    
+      contextMenuGroupId: 'autocomplete',
       contextMenuOrder: 1.5,
     
       // Method that will be executed when the action is triggered.
       // @param editor The editor instance is passed in as a convinience
       run: () => {
-        window.open('https://www.lelivrescolaire.fr')
+        this.enableAutoComplete(monaco)
+        return null;
+      }
+    });
+
+    editor.addAction({
+      // An unique identifier of the contributed action.
+      id: 'disable-autocomplete',
+      // A label of the action that will be presented to the user.
+      label: 'Disable autocomplete',
+      // An optional array of keybindings for the action.
+      keybindings: [],
+      // A precondition for this action.
+      precondition: null,
+      // A rule to evaluate on top of the precondition in order to dispatch the keybindings.
+      keybindingContext: null,
+      contextMenuGroupId: 'autocomplete',
+      contextMenuOrder: 1.5,
+    
+      // Method that will be executed when the action is triggered.
+      // @param editor The editor instance is passed in as a convinience
+      run: () => {
+        this.disableAutoComplete()
         return null;
       }
     });
